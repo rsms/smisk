aa7cf7ba4bf7987c0ebf8b6124a04e21ec18453d
Rasmus Andersson <rasmus@flajm.se>
2008-05-02 19:27:24 +0200

    Removed unnecessary content-type response header

--- examples/minimal/process.py
+++ examples/minimal/process.py
@@ -4,7 +4,6 @@ from smisk import Application
 
 class MyApp(Application):
   def service(self):
-    self.response.headers = ["Content-Type: text/html"]
     self.response.write("<h1>Hello World!</h1>")
 
 try:
