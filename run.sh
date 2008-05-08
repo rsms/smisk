22194a35e61b389b12109a0c5b9c3e8842f7a973
Rasmus Andersson <rasmus@flajm.se>
2008-05-08 16:36:28 +0200

    Better run script

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-path="$1"
-if [ "$path" == "" ]; then
-  path="examples/simple"
+cd $(dirname "$0")
+if [ $# -eq 0 ]; then
+  cd examples/simple
+else
+  if [ ! -f "$1/lighttpd.conf" ]; then
+    echo "$1 does not contain a lighttpd.conf file" >&2
+    exit 1
+  fi
+  cd "$1"
 fi
-cd $path
 echo 'Starting on http://localhost:8080/'
 lighttpd -Df lighttpd.conf
