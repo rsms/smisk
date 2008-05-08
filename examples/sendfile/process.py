488a3d5a98391139d2053958519eeff3a11d56c7
Rasmus Andersson <rasmus@flajm.se>
2008-05-08 17:58:29 +0200

    Corrected sendfile -> send_file (prototype changed)

--- examples/sendfile/process.py
+++ examples/sendfile/process.py
@@ -8,7 +8,7 @@ class MyApp(Application):
     if self.request.url.path[-4:] == '.jpg':
       path = os.path.abspath(self.request.url.path.replace('..', '').lstrip('/'))
       sys.stderr.write("Sending file %s\n" % path)
-      self.response.sendfile(path)
+      self.response.send_file(path)
     else:
       self.response.headers = ["Content-Type: text/html"]
       self.response.write("An image which will be sent using X-Sendfile if supported:<br/>")
