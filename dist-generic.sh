725b14231c1a273f7ff60b8b10103d963faeff67
Rasmus Andersson <rasmus@flajm.se>
2008-05-09 02:43:05 +0200

    Fixed generic dist script

--- dist-generic.sh
+++ dist-generic.sh
@@ -33,7 +33,7 @@ rm -vf dist/*.tar.gz
 rm -vf dist/ready/*.tar.gz
 mkdir -vp dist/ready
 
-BDIST_ID="$VERV-$(date '+%y%m%d')-$REV"
+DIST_ID="$VERV-$(date '+%y%m%d')-$REV"
 
 # Execute for each python environment
 for PYTHON in $@; do
@@ -42,7 +42,8 @@ for PYTHON in $@; do
   # Also, create source distribution:
   if [ "$HAS_PERFORMED_SDIST" == "" ]; then
     $PYTHON setup.py sdist --formats=gztar || exit 1
-    mv -v dist/$PACKAGE-$VER.tar.gz dist/ready/
+    SDIST_FILE="$(echo "$PACKAGE-$VER.tar.gz" | sed 's/'$PACKAGE'-'$VER'/'$PACKAGE'-'$DIST_ID'/g')"
+    mv -v "dist/$PACKAGE-$VER.tar.gz" "dist/ready/$SDIST_FILE"
     HAS_PERFORMED_SDIST=y
   fi
 
@@ -54,7 +55,7 @@ for PYTHON in $@; do
   PY_VER=$(echo $($PYTHON -V 2>&1) | sed 's/[^0-9\.]//g' | cut -d . -f 1,2)
   BDIST_FILE_ORG=$(echo dist/$PACKAGE-$VERV*.tar.gz)
   BDIST_FILE="$(echo "$BDIST_FILE_ORG" | sed 's/\.tar\.gz$/-py'$PY_VER'.tar.gz/g')"
-  BDIST_FILE="$(echo "$BDIST_FILE" | sed 's/'$PACKAGE'-'$VER'/'$PACKAGE'-'$BDIST_ID'/g')"
+  BDIST_FILE="$(echo "$BDIST_FILE" | sed 's/'$PACKAGE'-'$VER'/'$PACKAGE'-'$DIST_ID'/g')"
   mv -v $BDIST_FILE_ORG $BDIST_FILE || exit 1
   mv -v $BDIST_FILE dist/ready/ || exit 1
   
@@ -65,23 +66,24 @@ done # end of each python env
 $PYTHON setup.py apidocs
 
 # Upload & update links on server
-echo "Uploading dist/ready/$PACKAGE-$BDIST_ID*.tar.gz to $REMOTE_HOST"
 CMD="cd $REMOTE_PATH;\
-for f in $PACKAGE-$BDIST_ID*.tar.gz;do \
+for f in $PACKAGE-$DIST_ID*.tar.gz;do \
   if [ -f \"\$f\" ]; then\
-    lname=\`echo \"\$f\"|sed 's/$BDIST_ID/latest/g'\`;\
+    lname=\`echo \"\$f\"|sed 's/$DIST_ID/latest/g'\`;\
     ln -sf \"\$f\" \"\$lname\";\
   fi;\
 done"
 if is_local_host $REMOTE_HOST; then
-  cp dist/ready/$PACKAGE-$BDIST_ID*.tar.gz $REMOTE_PATH
-  $CMD
+  echo "Copying dist/ready/$PACKAGE-$DIST_ID*.tar.gz to $REMOTE_PATH"
+  cp dist/ready/$PACKAGE-$DIST_ID*.tar.gz $REMOTE_PATH
+  echo $CMD | sh --verbose
   if [ -d doc/api ]; then
     echo "Copying doc/api"
     cp -rf doc/api $REMOTE_PATH_DOCS
   fi
 else
-  scp -qC dist/ready/$PACKAGE-$BDIST_ID*.tar.gz $REMOTE_HOST:$REMOTE_PATH || exit 1
+  echo "Uploading dist/ready/$PACKAGE-$DIST_ID*.tar.gz to $REMOTE_HOST"
+  scp -qC dist/ready/$PACKAGE-$DIST_ID*.tar.gz $REMOTE_HOST:$REMOTE_PATH || exit 1
   ssh $REMOTE_HOST $CMD || exit 1
   if [ -d doc/api ]; then
     echo "Uploading doc/api to $REMOTE_HOST"
