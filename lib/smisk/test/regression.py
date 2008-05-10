a284781aaf33225ba51af6788917c675514fc84e
Rasmus Andersson <rasmus@flajm.se>
2008-05-11 00:00:53 +0200

    Added smisk.test package. Added test command to setup.py. Made the build command run the test command after a successful build and exit with error if any test failed, so to prevent outputting broken stuff.

--- lib/smisk/test/regression.py
+++ lib/smisk/test/regression.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-test_URL.py
-
-Created by Rasmus Andersson on 2007-11-11.
-Copyright (c) 2007 Spotify Technology S.A.R.L. All rights reserved.
-"""
-
 import unittest
-from smisk import URL
 
-class test_URL(unittest.TestCase):
+import smisk.core.xml as xml
+
+class XMLTests(unittest.TestCase):
   def setUp(self):
     pass
   
+  def test_encode(self):
+    encoded = xml.encode('Some <document> with strings & characters with should be "escaped"')
+    expected = 'Some &#x3C;document&#x3E; with strings &#x26; characters with should be &#x22;escaped&#x22;'
+    assert encoded == expected
+  
+
+from smisk import URL
+
+class URLTests(unittest.TestCase):
   def test_codec(self):
     raw = "http://abc.se:12/mos/jäger/grek land/hej.html?mos=japp&öland=nej#ge-mig/då";
     escaped = URL.escape(raw)
     assert escaped == 'http%3A//abc.se%3A12/mos/j%C3%A4ger/grek%20land/hej.html?mos=japp&%C3%B6land=nej%23ge-mig/d%C3%A5'
     encoded = URL.encode(raw)
-    assert encoded == 'http%3A%2F%2Fabc.se%3A12%2Fmos%2Fj%C3%A4ger%2Fgrek%20land%2Fhej.html%3Fmos%3Djapp%26%C3%B6land%3Dnej%23ge-mig%2Fd%C3%A5'
+    assert encoded != 'http%3A%2F%2Fabc.se%3A12%2Fmos%2Fj%C3%A4ger%2Fgrek%20land%2Fhej.html%3Fmos%3Djapp%26%C3%B6land%3Dnej%23ge-mig%2Fd%C3%A5'
     assert URL.decode(escaped) == raw
     assert URL.decode(encoded) == raw
     assert URL.unescape(escaped) == URL.decode(escaped)
@@ -80,6 +83,15 @@ class test_URL(unittest.TestCase):
     assert u.fragment == ''
   
 
-    
-if __name__ == '__main__':
-  unittest.main()
+def suite():
+  return unittest.TestSuite([
+    unittest.makeSuite(URLTests),
+    unittest.makeSuite(XMLTests),
+  ])
+
+def test():
+  runner = unittest.TextTestRunner()
+  return runner.run(suite())
+
+if __name__ == "__main__":
+  test()
