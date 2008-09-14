2bfa1850ea92aeda7be0bf7e3f14e260abb52f2b
Rasmus Andersson <rasmus@flajm.com>
2008-09-15 00:28:09 +0200

    Removed models init which is no longer needed

--- smisk_site/app/__init__.py
+++ smisk_site/app/__init__.py
@@ -5,8 +5,6 @@ from smisk.mvc import main
 import controllers
 
 if __name__ == '__main__':
-  models.setup_all()
-  models.create_all()
   main(
     appdir=os.path.dirname(os.path.dirname(__file__)),
     log_level=logging.DEBUG,
