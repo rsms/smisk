b45f60f06429f4fd50f79ffc034c15f4bc3eb090
Rasmus Andersson <rasmus@flajm.com>
2008-09-22 01:52:37 +0200

    Better example return value

--- examples/mvc/minimal/process.py
+++ examples/mvc/minimal/process.py
@@ -4,7 +4,7 @@ from smisk.mvc import Controller, main
 
 class Root(Controller):
   def __call__(self, *args, **params):
-    return "Hello World!\n"
+    return {'message': 'Hello World!'}
 
 if __name__ == '__main__':
   main()
