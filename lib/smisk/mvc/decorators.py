3dc11d91ff2d9d72144ad607ae637a093ddb0039
Rasmus Andersson <rasmus@flajm.com>
2008-10-08 00:17:33 +0200

    More precise testing of type in expose and hide decorators

--- lib/smisk/mvc/decorators.py
+++ lib/smisk/mvc/decorators.py
@@ -1,8 +1,7 @@
 # encoding: utf-8
-import sys
+import sys, types
 
 def expose(slug=None, template=None, formats=None):
-  #print >> sys.stderr, 'expose %r' % url_slug
   def entangle(func):
     if slug is not None and isinstance(slug, basestring):
       func.slug = str(slug)
@@ -18,17 +17,17 @@ def expose(slug=None, template=None, formats=None):
     
     return func
   
-  if slug is not None and not isinstance(slug, basestring):
+  if isinstance(slug, (types.FunctionType, types.MethodType)):
     return entangle(slug)
   return entangle
 
-def hide(x=None):
-  #print >> sys.stderr, 'hide %r' % a
+
+def hide(func=None):
   def entangle(func):
     func.hidden = True
     return func
   
-  if x is not None:
-    return entangle(x)
+  if isinstance(func, (types.FunctionType, types.MethodType)):
+    return entangle(func)
   return entangle
 
