6d9861f4f4a64e42ebace3e2d1d714377f09a699
Rasmus Andersson <rasmus@flajm.com>
2008-10-25 18:10:34 +0200

    Added DateTime, a sublcass of datetime, which handles time zones and can parse XML schema dateTime

--- lib/smisk/util.py
+++ lib/smisk/util.py
@@ -1,7 +1,9 @@
 # encoding: utf-8
 '''Miscellaneous utilities.
 '''
-import sys, os, time, threading, thread, logging, imp, inspect
+import sys, os, time, threading, thread, logging, imp, inspect, re
+from datetime import datetime, timedelta, tzinfo
+ZERO_TIMEDELTA = timedelta(0)
 try:
   import reprlib
 except ImportError:
@@ -10,14 +12,12 @@ from smisk.core import Application, URL
 from types import *
 
 None2 = (None, None)
-''':type: tuple'''
+''':type: tuple
+'''
 
-# Regex type
-import re
-re = re.compile('.')
-RegexType = type(re)
-del re
-
+RegexType = type(re.compile('.'))
+''':type: type
+'''
 
 _log = logging.getLogger(__name__)
 
@@ -339,6 +339,105 @@ repr2 = introspect._repr.repr
 
 :type: repr.Repr
 '''
+
+
+class UTCTimeZone(tzinfo):
+  '''UTC
+  '''
+  def __new__(cls):
+    try:
+      return cls._instance
+    except AttributeError:
+      cls._instance = tzinfo.__new__(UTCTimeZone)
+    return cls._instance
+  
+  def utcoffset(self, dt):
+    return ZERO_TIMEDELTA
+  
+  def tzname(self, dt):
+    return "UTC"
+  
+  def dst(self, dt):
+    return ZERO_TIMEDELTA
+  
+  def __repr__(self):
+    return 'UTCTimeZone()'
+  
+
+class TimeZone(tzinfo):
+  '''Fixed offset in minutes east from UTC.
+  '''
+  def __init__(self, tzstr_or_minutes):
+    if isinstance(tzstr_or_minutes, basestring):
+      minutes = (int(tzstr_or_minutes[1:3]) * 60) + int(tzstr_or_minutes[4:6])
+      if tzstr_or_minutes[0] == '-':
+        minutes = -minutes
+    else:
+      minutes = tzstr_or_minutes
+    self.__minute_offset = minutes
+    self.__offset = timedelta(minutes=minutes)
+  
+  def utcoffset(self, dt):
+    return self.__offset
+  
+  def dst(self, dt):
+    return ZERO_TIMEDELTA
+  
+  def __repr__(self):
+    return 'TimeZone(%d)' % self.__minute_offset
+  
+
+class DateTime(datetime):
+  '''Time zone aware version of datetime with additional parsers.
+  '''
+  XML_SCHEMA_DATETIME_RE = re.compile(r'((?#year)-?\d{4})-((?#month)\d{2})-((?#day)\d{2})T'\
+    r'((?#hour)\d{2}):((?#minute)\d{2}):((?#second)\d{2})((?#millis)\.\d+|)((?#tz)[+-]\d{2}:\d{2}|Z?)')
+  '''XML schema dateTime regexp.
+  
+  :type: RegexType
+  '''
+  def __new__(cls, dt=None, *args, **kwargs):
+    if isinstance(dt, datetime):
+      if isinstance(dt, cls):
+        return dt
+      return datetime.__new__(cls, 
+        dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second, dt.microsecond, dt.tzinfo)
+    return datetime.__new__(cls, dt, *args, **kwargs)
+  
+  def as_utc(self):
+    '''Return this date in Universal Time Coordinate
+    '''
+    if self.tzinfo is UTCTimeZone():
+      return self
+    dt = (self - self.utcoffset()).replace(tzinfo=UTCTimeZone())
+    return DateTime(dt)
+  
+  @classmethod
+  def now(self):
+    if time.timezone == 0 and time.daylight == 0:
+      tz = UTCTimeZone()
+    else:
+      tz = TimeZone(((-time.timezone)/60) + (time.daylight * 60))
+    return datetime.now().replace(tzinfo=tz)
+  
+  @classmethod
+  def parse_xml_schema_dateTime(cls, string):
+    '''Parse a XML Schema dateTime value.
+    
+    :see: `XML Schema Part 2: Datatypes Second Edition, 3.2.7 dateTime
+          <http://www.w3.org/TR/xmlschema-2/#dateTime>`__
+    '''
+    m = cls.XML_SCHEMA_DATETIME_RE.match(string).groups()
+    if m[7] and m[7] != 'Z':
+      tz = TimeZone(m[7])
+    else:
+      tz = UTCTimeZone()
+    microsecond = 0
+    if m[6]:
+      microsecond = int(float(m[6]) * 1000000.0)
+    dt = DateTime(int(m[0]), int(m[1]), int(m[2]), int(m[3]), int(m[4]), int(m[5]), microsecond, tz)
+    return dt
+  
 
 
 def callable_cache_key(node):
