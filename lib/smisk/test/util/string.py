1aecbea2a0351feaea12299b0894e9224cff692e
Rasmus Andersson <rasmus@flajm.com>
2008-10-26 04:54:11 +0100

    Added test for smisk.util.string

---
+++ lib/smisk/test/util/string.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from smisk.test import *
+from smisk.util.string import *
+from smisk.core import URL
+
+class StringUtilTests(TestCase):
+  def test1_normalize_url(self):
+    abs_url = URL('http://www.foo.tld/bar/?arg=12&baz=abc')
+    self.assertEquals(normalize_url('/mos', abs_url), 'http://www.foo.tld/mos')
+    self.assertEquals(normalize_url('mos.html', abs_url), 'http://www.foo.tld/bar/mos.html')
+    self.assertEquals(normalize_url('mos.html?xyz=987&abc=123', abs_url), 'http://www.foo.tld/bar/mos.html?xyz=987&abc=123')
+    self.assertEquals(normalize_url('mos.html'), '/mos.html')
+    self.assertEquals(normalize_url('/mos'), '/mos')
+  
+
+def suite():
+  return unittest.TestSuite([
+    unittest.makeSuite(StringUtilTests),
+  ])
+
+def test():
+  runner = unittest.TextTestRunner()
+  return runner.run(suite())
+
+if __name__ == "__main__":
+  test()
