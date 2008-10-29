710b3c00623964e0df7f0c01dd95d6f0c13e90a0
Rasmus Andersson <rasmus@flajm.com>
2008-10-30 00:37:11 +0100

    Debian updates

--- admin/dist-debian.sh
+++ admin/dist-debian.sh
@@ -1,33 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Build and distribute Debian packages
 #
 
+RUN_DUPLOAD=0
 DEB_BRANCH='unstable'
-DISTRIBUTE=0
 DEB_REMOTE_HOST='rasmus@hunch.se'
 DEB_REMOTE_PATH='/var/www/hunch.se/www/public/debian/'
 
 # ----------------------------------
 
 usage() { (cat <<USAGE
-Usage: $0 [-u]
+Usage: $0 [-u] [options to dpkg-buildpackage]
   Build Debian packages [and upload to hunch Debian repository].
 Options:
-  -u  Upload the resulting packages to the hunch Debian repository.
+  -u  Upload the resulting packages to the hunch Debian repository using dupload.
 USAGE
   ) >&2
 }
 
 # Take care of arguments
+args=( $* )
 if [ $# -gt 0 ]; then
   if [ "$1" = "-u" ]; then
-    DISTRIBUTE=1
-  else
+    RUN_DUPLOAD=1
+    args=${args[@]:1}
+  elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     usage ; exit 1
   fi
 fi
-
 
 # Ensure we're on Debian and has package builder
 if [ $(uname -s) != "Linux" ] || [ ! -x /usr/bin/dpkg-buildpackage ]; then
@@ -35,147 +36,34 @@ if [ $(uname -s) != "Linux" ] || [ ! -x /usr/bin/dpkg-buildpackage ]; then
   exit 1
 fi
 
-
 # Properties
 cd "$(dirname "$0")/.."
 . admin/dist-base.sh || exit 1
-
+UPSTREAM_VER=$VER
+PREV_UPSTREAM_VER=$(head -n 1 debian/changelog | cut -d ' ' -f 2 | cut -d - -f 1)
 DEB_PACKAGE_NAME="python-$PACKAGE"
-CURRENT_VER=$VER  # just gives this a better name
-PREV_VER=         # 1.0.1
-PREV_PKGVER=      # 7
-DEB_PACKAGE_VER=  # 1
-
-
-# Get info about the latest version from the changelog
-for r in $(grep -E "${DEB_PACKAGE_NAME} "'\(.+-[0-9]+\)' debian/changelog | cut -d ' ' -f 2 | sed -r 's/(\(|\))//g'); do
-  if [ -z $PREV_VER ]; then
-    PREV_VER=$(echo $r|cut -d - -f 1)
-    PREV_PKGVER=$(echo $r|cut -d - -f 2)
-  fi
-done
-
+DEB_REVISION=$(head -n 1 debian/changelog | cut -d ' ' -f 2 | cut -d - -f 2)
+DEB_BRANCH=$(head -n 1 debian/changelog | cut -d ' ' -f 3 | sed 's/;//')
 
 ensure_clean_working_revision
 
-
-if [ "$PREV_VER" = "$CURRENT_VER" ]; then
-  echo 'The program version AND package version seems to be up to date in '
-  echo 'the changelog. Make sure you have updated it. (For example the '
-  echo "debian package version which is now ${PREV_PKGVER})"
-  read -n 1 -p 'Build package with current changelog? [Y/n] ' ANSWER
-  if [ "$ANSWER" != "" ] && [ "$ANSWER" != "y" ] && [ "$ANSWER" != "Y" ]; then
-    echo
-    echo 'Aborted by user' >&2
-    exit 1
-  fi
-  DEB_PACKAGE_VER=$PREV_PKGVER
-  # continue with unmodified changelog
-else
-  # changelog probably out of date - ensure it's updated
-  SKIP_CHANGELOG=0
-  LESS=$(which less)
-  if [ "$LESS" = "" ]; then LESS=$(which more); fi
-  if [ "$LESS" = "" ]; then LESS=$(which cat); fi
-
-  echo 'The debian/changelog needs to be updated.
--------------------------------
-You can view a more detailed revision changelog here:
-http://trac.hunch.se/${PACKAGE?}/log
-Note: Editing the changelog through this interface will case a RSC commit.
--------------------------------'
-  NEED_ANSWER=1
-  while [ $NEED_ANSWER -eq 1 ]; do
-    echo '[1] Help me edit debian/changelog and continue.
-[2] Contiune without modifying the changelog. (Not recommended)
-CTRL+C to abort.'
-    read -n 1 -p 'Enter your choice [1-2]: (1) ' ANSWER
-    if [ "$ANSWER" = "" ]; then ANSWER=1; fi
-    case $ANSWER in
-      1) NEED_ANSWER=0 ;;
-      2) NEED_ANSWER=0; SKIP_CHANGELOG=1 ;;
-      *) ;;
-    esac
-  done
-
-  if [ $SKIP_CHANGELOG -eq 0 ]; then
-    
-    # Make sure the version is changed
-    if [ "$PREV_VER" != "$CURRENT_VER" ]; then
-      DEB_PACKAGE_VER=1
-      echo "Package version reset to 1 due to new program version"
-    else
-      DEB_PACKAGE_VER=$(expr $DEB_PACKAGE_VER + 1)
-      echo "Package version bumped up to $DEB_PACKAGE_VER due to unchanged program version"
-    fi
-    
-    # Construct a changelog message
-    for r in $(grep -E '^ -- ' debian/changelog|sed -r 's/ -- (.+<[^>]+>).+/\1/g'|sed 's/ /___/g'); do
-      if [ "$PREV_DEB_CONTACT" = "" ]; then
-        PREV_DEB_CONTACT=$(echo "$r"|sed 's/___/ /g');
-      fi
-    done
-    echo "${DEB_PACKAGE_NAME} (${CURRENT_VER}-${DEB_PACKAGE_VER}) ${DEB_BRANCH}; urgency=low" > debian/changelog.tmp
-    echo >> debian/changelog.tmp
-    echo '  * '>> debian/changelog.tmp
-    echo >> debian/changelog.tmp
-    echo " -- $PREV_DEB_CONTACT  $(date --rfc-2822)">> debian/changelog.tmp
-    echo >> debian/changelog.tmp
-    cat debian/changelog >> debian/changelog.tmp || exit 1
-    
-    
-    # Let the user modify and update the changelog
-    NEED_ANSWER=1
-    while [ $NEED_ANSWER -eq 1 ]; do
-      SUM_BEFORE=$(md5sum debian/changelog.tmp|cut -d ' ' -f 1)
-      $EDITOR debian/changelog.tmp
-      SUM_AFTER=$(md5sum debian/changelog.tmp|cut -d ' ' -f 1)
-      if [ "$SUM_AFTER" = "$SUM_BEFORE" ]; then
-        echo 'The debian/changelog message unchanged or not specified.'
-        read -n 1 -p 'a)bort, c)ontinue, e)dit: (e) ' ANSWER
-        case $ANSWER in
-          a) rm debian/changelog.tmp ; exit 2 ;;
-          c) NEED_ANSWER=0 ;;
-          *) ;;
-        esac
-      else
-        NEED_ANSWER=0
-      fi
-    done
-    mv debian/changelog.tmp debian/changelog || exit 1
-    echo 'debian/changelog updated'
-    
-    
-    # Commit changes to the changelog
-    if [ -d .svn ]; then
-      echo 'Committing changelog update'
-      svn ci -m 'Debian changelog updated (dist-debian.sh)' debian/changelog
-      svn up
-    elif [ -d .hg ]; then
-      echo 'Committing changelog update'
-      hg ci -m 'Debian changelog updated (dist-debian.sh)' debian/changelog
-    fi
-    ensure_clean_working_revision
-    
-  fi # [ $SKIP_CHANGELOG -eq 0 ]
-  
-fi # [ "$PREV_VER" = "$CURRENT_VER" ] && [ "$PREV_PKGVER" = "$DEB_PACKAGE_VER" ] else
+if [ "$PREV_UPSTREAM_VER" != "" ]; then
+  echo "Error: Changelog out of date" >&2
+  echo "Run dch -v $UPSTREAM_VER-"$(expr $DEB_REVISION + 1)" and write a new changelog entry."
+  exit 1
+fi
 
 # Build
 echo 'Running dpkg-buildpackage -rfakeroot'
-dpkg-buildpackage -rfakeroot -us -uc || exit 1
-
+dpkg-buildpackage -rfakeroot ${args} || exit 1
 
 # Move files to a better location
-# XXX: This should dpkg-buildpackage be able to do. Checked it up quickly but did not find anything.
-FNPATTERN="${DEB_PACKAGE_NAME}_${CURRENT_VER}-${DEB_PACKAGE_VER}"
 rm -rf dist/debian || exit 1
 mkdir -vp dist/debian || exit 1
-mv -v ../$FNPATTERN* dist/debian/ || exit 1
+mv -v ../${DEB_PACKAGE_NAME}_${UPSTREAM_VER}-${DEB_REVISION}* dist/debian/ || exit 1
 
-
-# Distribute
-if [ $DISTRIBUTE -eq 1 ]; then
-  echo "Running dupload -t hunch.se dist/debian"
-  dupload -t hunch.se dist/debian
+# Upload
+if [ $RUN_DUPLOAD -eq 1 ]; then
+  echo "Running dupload -t hunch.se-${DEB_BRANCH} dist/debian"
+  dupload -t hunch.se-${DEB_BRANCH} dist/debian
 fi
