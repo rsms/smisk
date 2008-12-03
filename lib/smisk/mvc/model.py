e11f696935ec2927f83042562df69710618399b6
Rasmus Andersson <rasmus@flajm.com>
2008-12-04 00:53:10 +0100

    fixed log instance ref

--- lib/smisk/mvc/model.py
+++ lib/smisk/mvc/model.py
@@ -2,22 +2,24 @@
 '''Model in MVC
 '''
 from warnings import filterwarnings, warn
+import logging
+log = logging.getLogger(__name__)
 try:
   # Ignore the SA string type depr warning
   from sqlalchemy.exceptions import SADeprecationWarning
   filterwarnings('ignore', 'Using String type with no length for CREATE TABLE',
                  SADeprecationWarning)
-
+  
   # Import Elixir & SQLAlchemy
   import elixir as _elixir
   from elixir import *
   from sqlalchemy import func
   import sqlalchemy as sql
-
+  
   # Disable autosetup by recommendation from Jason R. Coombs:
   # http://groups.google.com/group/sqlelixir/msg/ed698d986bfeefdb
   options_defaults['autosetup'] = False
-
+  
   # Control wheretere to include module name or not in table names.
   # If True, project.fruits.Apple -> table apples.
   # If False, project.fruits.Apple -> table project_fruits_apples.
@@ -25,16 +27,18 @@ try:
   
   # Metadata configuration bind filter
   from smisk.config import config
-  import logging
-  log = logging.getLogger(__name__)
   def smisk_mvc_metadata(conf):
+    global log
     dburi = conf.get('smisk.mvc.model.bind')
     if dburi is not None:
-      log.info('binding to %r', dburi)
+      from smisk.core import URL
+      dburl = URL(dburi)
+      if dburl.password:
+        dburl.password = '***'
+      log.info('binding to %r', str(dburl))
       metadata.bind = dburi
   config.add_filter(smisk_mvc_metadata)
   # dont export these
-  del log
   del smisk_mvc_metadata
   del config
   
