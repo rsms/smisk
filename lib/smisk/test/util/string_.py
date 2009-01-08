4897d4ce79c48103682b34a9dc6fb834f58058c9
Rasmus Andersson <rasmus@flajm.com>
2009-01-08 03:49:48 +0100

    fixed test that broke after updating impl of normalize_url

--- lib/smisk/test/util/string_.py
+++ lib/smisk/test/util/string_.py
@@ -7,11 +7,11 @@ from smisk.core import URL
 class StringUtilTests(TestCase):
   def test1_normalize_url(self):
     abs_url = URL('http://www.foo.tld/bar/?arg=12&baz=abc')
-    self.assertEquals(normalize_url('/mos', abs_url), 'http://www.foo.tld/mos')
-    self.assertEquals(normalize_url('mos.html', abs_url), 'http://www.foo.tld/bar/mos.html')
-    self.assertEquals(normalize_url('mos.html?xyz=987&abc=123', abs_url), 'http://www.foo.tld/bar/mos.html?xyz=987&abc=123')
-    self.assertEquals(normalize_url('mos.html'), '/mos.html')
-    self.assertEquals(normalize_url('/mos'), '/mos')
+    self.assertEquals(normalize_url('/mos', abs_url).__str__(), 'http://www.foo.tld/mos')
+    self.assertEquals(normalize_url('mos.html', abs_url).__str__(), 'http://www.foo.tld/bar/mos.html')
+    self.assertEquals(normalize_url('mos.html?xyz=987&abc=123', abs_url).__str__(), 'http://www.foo.tld/bar/mos.html?xyz=987&abc=123')
+    self.assertEquals(normalize_url('mos.html').__str__(), '/mos.html')
+    self.assertEquals(normalize_url('/mos').__str__(), '/mos')
   
 
 def suite():
