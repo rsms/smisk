c2a1b9158ceedcaacb8576ca5c6a78ecf3f8efc2
Rasmus Andersson <rasmus@flajm.com>
2009-01-11 19:32:42 +0100

    year in copyright 2008 -> 2009

--- lib/smisk/release.py
+++ lib/smisk/release.py
@@ -5,8 +5,8 @@ __all__ = ['version','author','email','copyright','license','version_info']
 version = "1.1.1" # Major.Minor.Build (see tag_build in setup.cfg)
 author = "Rasmus Andersson"
 email = "rasmus@flajm.com"
-copyright = "Copyright 2007-2008 Rasmus Andersson"
-license = r'''Copyright (c) 2007-2008 Rasmus Andersson
+copyright = "Copyright 2007-2009 Rasmus Andersson"
+license = r'''Copyright (c) 2007-2009 Rasmus Andersson
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
