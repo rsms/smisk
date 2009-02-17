201fcff8a5c8326ae2503276d0254dc0f4b3b3ee
Rasmus Andersson <rasmus@flajm.com>
2009-02-17 03:20:39 +0100

    more explanative messages when uploading documentation

--- admin/docsdist.sh
+++ admin/docsdist.sh
@@ -17,8 +17,9 @@ rm -rf docs/html || exit 1
 VER=$(./setup.py --version)
 REMOTEDIR="/var/www/python-smisk.org/www/public/docs"
 TFN=".uploading-$(date '+%y%m%d-%H%M%S')-$VER"
-echo "Uploading HTML docs to python-smisk.org:$REMOTEDIR/..."
+echo "Uploading HTML docs to python-smisk.org:$REMOTEDIR/$TFN"
 scp -Cr "docs/html" "python-smisk.org:$REMOTEDIR/$TFN"
+echo "Upload done. Staging new files $REMOTEDIR/$TFN -> $REMOTEDIR/$VER"
 ssh python-smisk.org "cd $REMOTEDIR && rm -rf $VER && mv -f $TFN $VER"
 
 echo "If you wish to make this documentation the current one, do this:"
