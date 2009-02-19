c55b08883aa04ab704d83bb94020c07951257c48
Rasmus Andersson <rasmus@flajm.com>
2009-02-19 03:35:37 +0100

    updated the kittens-orm example app with html templates and some other stuff

--- examples/mvc/kittens_orm/app.py
+++ examples/mvc/kittens_orm/app.py
@@ -2,31 +2,48 @@
 # encoding: utf-8
 from smisk.mvc import *
 from smisk.mvc.model import *
+from smisk.serialization import xmlgeneric
+import logging
+
+log = logging.getLogger(__name__)
 
 class Kitten(Entity):
   name = Field(Unicode(255), primary_key=True)
-  color = Field(Unicode(30))
-  year_born = Field(Integer)
+  color = Field(Unicode(30), required=True, default=u'purple')
+  year_born = Field(Integer, required=True, default=0)
 
 class root(Controller):
   def __call__(self, *args, **params):
-    kittens = [dict(k) for k in Kitten.query.all()]
-    return {'kittens': kittens}
-
+    log.info('listing all kittens')
+    return {'kittens': Kitten.query.all()}
+  
   def create(self, name, color=None, year_born=None):
+    name = name.strip()
+    if not name:
+      raise http.BadRequest('name attribute must not be empty')
     kitten = Kitten(name=name, color=color, year_born=year_born)
+    log.info('created kitten: %r', kitten)
     redirect_to(self.read, kitten)
   
   def read(self, name):
+    log.info('reading kitten %r', name)
     kitten = Kitten.get_by(name=name)
-    return kitten.to_dict()
+    if not kitten:
+      raise http.NotFound()
+    return {'kitten': kitten}
   
-  def update(self, name, **params):
-    kitten = Kitten.get_by(name=name)
+  def update(self, name, original_name=None, **params):
+    if original_name and original_name != name:
+      kitten = Kitten.get_by(name=original_name)
+      kitten.name = name
+    else:
+      kitten = Kitten.get_by(name=name)
     kitten.from_dict(params)
+    log.info('updated kitten %r', kitten)
     redirect_to(self.read, kitten)
   
   def delete(self, name):
+    log.info('deleting kitten %r', name)
     kitten = Kitten.get_by(name=name)
     kitten.delete()
     redirect_to(self)
