2409e57af2747ece27632a54666d9ad23448943b
Rasmus Andersson <rasmus@flajm.com>
2009-02-23 01:54:43 +0100

    added document giving an overview of the release/distribution process

---
+++ docs/etc/distprocess.sh
@@ -0,0 +1,20 @@
+# The distribution process
+
+# Tag the node
+git tag -s v1.2.3
+
+# Debian
+# On each Debian build machine:
+git clone rasmus@vmware-host:src/smisk smisk
+cd smisk
+./setup.py debian -b
+# On ONE of the Debian build machines, also create source package:
+./setup.py debian -b -s
+dupload -t hunch dist/debian
+
+# Source
+./admin/sdist.sh --upload # python-smisk.org
+./setup.py sdist upload --sign # PyPI
+
+# MacPorts (Need to be done after source has been distributed)
+# todo
