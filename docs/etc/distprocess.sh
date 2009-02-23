e1af5745c077d968a15ec359a66780c1184c09c5
Rasmus Andersson <rasmus@flajm.com>
2009-02-23 03:08:19 +0100

    added macports and other stuff to distprocess doc

--- docs/etc/distprocess.sh
+++ docs/etc/distprocess.sh
@@ -16,8 +16,21 @@ dupload -t hunch dist/debian
 ./admin/sdist.sh --upload # python-smisk.org
 ./setup.py sdist upload --sign # PyPI
 
-# MacPorts (Need to be done after source has been distributed)
-# todo
+# MacPorts
+# Update version number
+# Update port revision
+# Update checksums (copy from http://python-smisk.org/dist/)
+# Commit changes upstream
+
+# Documentation
+./admin/docsdist.sh
 
 # Push tags and possible changes to the master repository
 git push --tags
+
+# Update the smisk website http://python-smisk.org/
+# Update version in app.py
+
+# Bump the version number
+# Update lib/smisk/release.py
+# Add "dev" build tag to setup.cfg
